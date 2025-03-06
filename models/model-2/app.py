c37d631a12376a3c582ce190bebbf6e42f4e8fcd
bharat15-ml <bharatbhushansingh2019@gmail.com>
2025-03-06 04:41:12 -0800

    Added required files for simple linear regression model

---
+++ models/model-2/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# Load the model
+with open('movie_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()  # Get data from the request
+
+    # Extract features from the data
+    city = data['city']
+    movie_type = data['type']
+    avg_ticket_price = data['average_ticket_price']
+    profit_margin = data['profit_margin']
+
+    # Create a DataFrame for prediction
+    input_data = pd.DataFrame({'city': [city], 'type': [movie_type], 'average_ticket_price': [avg_ticket_price], 'profit_margin': [profit_margin]})
+
+    # Make prediction
+    prediction = model.predict(input_data)[0]
+
+    # Return the prediction as JSON
+    return jsonify({'predicted_seats': prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
